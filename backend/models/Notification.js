f9e0bd9d5266641c12eeca2c62616321206340cf
Suraj <mahatosuraj6224@gmail.com>
2026-08-27 23:08:40 +0530

    Create Notification.js

---
+++ backend/models/Notification.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        'request',
+        'approved',
+        'rejected',
+        'pickup',
+        'delivered',
+        'general'
+      ],
+      default: 'general',
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+
+    requestId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Request',
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Notification', notificationSchema);
