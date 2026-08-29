18d2aee19561c1b76f238484bdf27fee34c2c4c3
Suraj <mahatosuraj6224@gmail.com>
2026-08-29 18:00:07 +0530

    Add notification routes to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middlewares/errorMiddleware');
+const notificationRoutes = require('./routes/notification');
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/food', require('./routes/food'));
 app.use('/api/request', require('./routes/request'));
 app.use('/api/admin', require('./routes/admin'));
+app.use('/api/notification', notificationRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
